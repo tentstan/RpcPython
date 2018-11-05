0c6683dffbe7de37c792d6e2446b70dfb556348e
tanhl <tanhl@163.com>
2018-11-05 19:32:18 +0800

    motify __main__ to __name__

--- Server.py
+++ Server.py
@@ -39,7 +39,7 @@ def send_result(conn,out,result):
 	conn.send_all(length_prefix)
 	conn.send_all(response)
 
-if __main__ == '__main__':
+if __name__ == '__main__':
 	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 	sock.bind("localhost",8080)
